9ca4a355432eefc227a3ea205df4e4e095e2de95
LiuZihao <1099169475@qq.com>
2016-03-31 08:30:46 +0800

    no message

--- homework5/findpath/game.ts
+++ homework5/findpath/game.ts
@@ -1,91 +1,30 @@
+
 module game {
 
 
-    const GRID_PIXEL_WIDTH = 50;
+}
 
-    const GRID_PIXEL_HEIGHT = 50;
+var humanContainer = new render.DisplayObjectContainer();
+var head = new render.Bitmap();
+head.source = "wander-icon.jpg";
+humanContainer.addChild(head)
 
-    const NUM_ROWS = 12;
-
-    const NUM_COLS = 12;
-
-    export class WorldMap extends DisplayObject {
-
-
-        public grid: astar.Grid;
-        constructor() {
-            super();
-            var grid = new astar.Grid(NUM_COLS, NUM_ROWS);
-            this.grid = grid;
-            grid.setWalkable(5, 0, false);
-            grid.setWalkable(5, 1, false);
-            grid.setWalkable(5, 2, false);
-            grid.setWalkable(5, 3, false);
-            grid.setWalkable(5, 4, false);
-            grid.setWalkable(5, 5, false);
-
-        }
-
-        render(context: CanvasRenderingContext2D) {
-            context.fillStyle = '#0000FF';
-            context.strokeStyle = '#FF0000';
-            context.beginPath();
-            for (var i = 0; i < NUM_COLS; i++) {
-                for (var j = 0; j < NUM_ROWS; j++) {
-                    context.rect(i * GRID_PIXEL_WIDTH, j * GRID_PIXEL_HEIGHT, GRID_PIXEL_WIDTH, GRID_PIXEL_HEIGHT);
-                    context.fill();
-                    context.stroke();
-                }
-            }
-            context.closePath();
-
-        }
-
-    }
-
-    export class BoyShape extends DisplayObject {
-        render(context: CanvasRenderingContext2D) {
-            context.beginPath()
-            context.fillStyle = '#00FFFF';
-            context.arc(GRID_PIXEL_WIDTH / 2, GRID_PIXEL_HEIGHT / 2, Math.min(GRID_PIXEL_WIDTH, GRID_PIXEL_HEIGHT) / 2 - 5, 0, Math.PI * 2);
-            context.fill();
-            context.closePath();
-        }
-    }
-
-    export class BoyBody extends Body {
+var renderCore = new render.RenderCore();
+renderCore.start(humanContainer, ["wander-icon.jpg"]);
 
 
-        public run(grid) {
-            grid.setStartNode(0, 0);
-            grid.setEndNode(10, 8);
-            var findpath = new astar.AStar();
-            findpath.setHeurisitic(findpath.diagonal);
-            var result = findpath.findPath(grid);
-            var path = findpath._path;
-            console.log(path);
-            console.log(grid.toString());
-        }
+class HumanBody extends Body {
 
-        public onTicker(duringTime) {
 
-        }
+    onTicker(duringTime: number) {
+
+        // this.x = 
+        // this.y = 
+        // this.rotation =
+
     }
 }
 
-
-
-
-var boyShape = new game.BoyShape();
-var world = new game.WorldMap();
-var body = new game.BoyBody(boyShape);
-body.run(world.grid);
-
-
-var renderCore = new RenderCore();
-renderCore.start([world, boyShape]);
-
 var ticker = new Ticker();
+var body = new HumanBody(humanContainer);
 ticker.start([body]);
-
-alert(navigator['serviceWorker'])
