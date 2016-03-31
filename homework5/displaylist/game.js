b7a88f1dc9ac467fdeb9ef6c99779ce3a8c10544
LiuZihao <1099169475@qq.com>
2016-03-31 08:22:50 +0800

    try

---
+++ homework5/displaylist/game.js
@@ -0,0 +1,27 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var humanContainer = new render.DisplayObjectContainer();
+var head = new render.Bitmap();
+head.source = "wander-icon.jpg";
+humanContainer.addChild(head);
+var renderCore = new render.RenderCore();
+renderCore.start(humanContainer, ["wander-icon.jpg"]);
+var HumanBody = (function (_super) {
+    __extends(HumanBody, _super);
+    function HumanBody() {
+        _super.apply(this, arguments);
+    }
+    HumanBody.prototype.onTicker = function (duringTime) {
+        // this.x = 
+        // this.y = 
+        // this.rotation =
+    };
+    return HumanBody;
+}(Body));
+var ticker = new Ticker();
+var body = new HumanBody(humanContainer);
+ticker.start([body]);
+//# sourceMappingURL=game.js.map
